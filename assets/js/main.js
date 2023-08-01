7422175381296ffafb13c4c733577328a8e5f3ef
Aldrin1807 <ai58914@ubt-uni.net>
2023-08-01 22:21:08 +0200

    all done

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,3 +6,29 @@ function errorSweetAlert(message) {
         confirmButtonText: 'OK'
     });
 }
+function logout(bool){
+
+    sessionStorage.removeItem("token");
+
+    if(bool){
+        window.location.href = "pages/login.html";
+    }else{
+    
+    window.location.href = "login.html";
+    }
+}
+
+
+const token = sessionStorage.getItem('token');
+        const loginSignup = document.getElementById('loginSignup');
+        const butoni = document.getElementById('buttoni');
+
+        if(token){
+            loginSignup.style.display = 'none';
+        }else{
+            butoni.style.display = 'none';
+        }
+
+
+
+        
